ba41e46eddecfeea9d1d9ceb7b09814ec4ab106c
dmaberlin1 <dmaberlin77@gmail.com>
2023-04-05 22:41:28 +0300

    feat: extended  JS lessons

--- app.js
+++ app.js
@@ -1,5 +1,134 @@
 'use strict'
 
+// **********************Расчеты и Math***********************************
+//
+// console.log(Math.cbrt(27))
+// console.log(Math.ceil(27 ** (1/4)))
+//
+// const numMinus= -1;
+// const numPositive=1
+// console.log(numMinus*100)
+// console.log(numPositive*100)
+// console.log(Math.abs(numMinus))
+//
+// console.log(Math.max(100,400,2,'500'))
+// console.log(Math.min(100,2,'20'))
+// const nums=[100,140,'160']
+// console.log(Math.max(...nums))
+// console.log(Math.trunc(2.22222242424))
+// console.log((4444.414141).toFixed(1))
+// console.log(Number((1.444444).toFixed(2)))
+//
+//
+
+//фция которая принимает мин и макс число и возвращает целое случайное между ними
+//
+// const randomNum = (min,max) => {
+//     return Math.floor((Math.random()*(max-min+1))+min)
+// }
+// console.log(randomNum(1,500))
+//
+// console.log(15/2)
+// console.log(15%2)
+// console.log(14%2)
+//
+// const isEven=n=>n%2===0;
+// const isOdd=n=>n%2===1;
+// console.log(isEven(11))
+// console.log(isOdd(11))
+//
+// const bigNum=2_000_000
+// const payment=3_50
+// console.log(+'200_500_500')
+//
+//
+// //операции до этого числа являются безопасными и корректными
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(2**53 -1)
+//
+//
+//
+
+
+// ************** Интернационализация******
+
+const options1= {
+    style: 'currency',
+    currency: 'UAH',
+}
+const options01= {
+    style: 'currency',
+    currency: 'UAH',
+    useGrouping:false
+}
+const options2={
+    style:'currency',
+    currency:'USD',
+    // useGrouping:false
+
+}
+const options3={
+    style:'decimal',
+}
+const options4={
+    style:'percent',
+
+}
+const options5={
+    style:'unit',
+    unit:'celsius'
+
+}
+
+console.log(new Intl.NumberFormat('ru-RU',options01).format(250000))
+console.log(new Intl.NumberFormat('ru-RU',options1).format(250000))
+console.log(new Intl.NumberFormat('en-US',options1).format(250000))
+console.log(new Intl.NumberFormat('en-US',options2).format(250000))
+console.log(new Intl.NumberFormat('ru-RU',options2).format(250000))
+
+console.log(new Intl.NumberFormat('ru-RU',options3).format(2500000))
+console.log(new Intl.NumberFormat('ru-RU',options4).format(0.41))
+console.log(new Intl.NumberFormat('ru-RU',options5).format(25))
+
+
+
+end in 9 less
+
+
+
+
+
+
+
+
+
+// console.log(Number.parseInt('100lolito',10))
+// // можно обратиться и без прототипа Number, лучше с ним , более явно будет указано
+// // console.log(Number.isNaN('10'))
+// console.log(Number.isFinite(100/0))
+// console.log(Number.isInteger(100.1))
+// //isFinite хороший способ проверки
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ************** Set & Map ***** Сеты и Мэпы*****
+
 // const flights=['Ukraine','NY','London','Paris']
 //
 // const setFlights=new Set(flights)
@@ -16,6 +145,11 @@
 // //разобрали sets в обычный массив
 // console.log([...setFlights])
 
+
+
+
+// Map это коллекция, пар ключ\значение, может использовать любой тип данных в качестве ключа,
+//   и поддерживает порядок своих записех
 
 // const weatherMap=new Map();
 // weatherMap.set('London','22-30').set('Kiev','21-00')
@@ -39,4 +173,52 @@
 // console.log(weatherMap.get([1,2,3]))
 // console.log(weatherMap.get(nums))
 
-
+//быстрое создание map
+//
+// const weatherMap=new Map([
+//     ['Kiev','22-30'],
+//     ['Lviv','21-30'],
+// ])
+//
+// console.log(weatherMap)
+//
+// const weatherObj={
+//     "Kiev":15,
+//     "Lvov":14,
+//     "Chernovsty":16
+// }
+//
+// console.log(Object.entries(weatherObj))
+// const weatherNewMap=new Map(Object.entries(weatherObj))
+// console.log(weatherNewMap.size)
+// //быстрый способ получить размер обьекта, конвертировать его в map и вызвать .size
+//
+// for(const [key,value] of weatherNewMap) {
+//     console.log(` key :${key}  value ${value}`)
+// }
+//
+// //вернули обратно в  обычный массив массивов
+// console.log([...weatherNewMap])
+// console.log([...weatherNewMap.keys()])
+// console.log([...weatherNewMap.values()])
+//
+//
+// const weatherMap=new Map([
+//   ['Miami',10],
+//   ['NY',7],
+//   ['California',14],
+// ])
+//
+// const weatherObj=[...weatherMap]
+// console.log(Object.entries(weatherObj))
+//
+// let weatherNewMap=new Map()
+// for (const [key,value] of weatherObj){
+//     console.log(key)
+//     console.log(value)
+//     weatherNewMap.set(value,key)
+// }
+// console.log(weatherNewMap)
+//
+// weatherNewMap=new Map([...weatherNewMap].map(el=>el.reverse()))
+// console.log(weatherNewMap)
