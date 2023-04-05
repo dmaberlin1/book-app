fae4fe952cdef0215b436df37a3ee9b956a0c58f
dmaberlin1 <dmaberlin77@gmail.com>
2023-04-05 14:16:27 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+'use strict'
+
+// const flights=['Ukraine','NY','London','Paris']
+//
+// const setFlights=new Set(flights)
+// console.log(setFlights.size)
+// console.log(setFlights.has('NY'))
+// console.log(setFlights.has('f'))
+// setFlights.add('Kiev')
+// setFlights.delete('NY')
+// console.log(setFlights)
+//
+// for(const flight of setFlights){
+//     console.log(flight)
+// }
+// //разобрали sets в обычный массив
+// console.log([...setFlights])
+
+
+// const weatherMap=new Map();
+// weatherMap.set('London','22-30').set('Kiev','21-00')
+//
+// console.log(weatherMap.has('London'))
+// console.log(weatherMap.get('London'))
+// weatherMap.set('London','22-45')
+// console.log(weatherMap.get('London'))
+// weatherMap.delete('London')
+// weatherMap.clear()
+//
+// const nums=[10,20,30]
+//
+// weatherMap.set(1,'one').set(true,'yes').set(false,'no').set(false,'no!')
+//   .set([1,2,3],'123').set({a:1},{a:'one'}).set(nums,'ten,twenty,thirty')
+//
+// /*обьекты и массивы в качестве ключей используются крайне редко, но важно помнить про
+// сайд эффект, и создание в памяти обьекта, и храним мы обьект по рефференсу,ссылке на него, по ячейке в памяти */
+// console.log(weatherMap)
+// console.log(weatherMap.size)
+// console.log(weatherMap.get([1,2,3]))
+// console.log(weatherMap.get(nums))
+
+
