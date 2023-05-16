cf090fb8923c8e3ad043e37b96826fc645359e5c
dmaberlin1 <dmaberlin77@gmail.com>
2023-05-16 17:58:52 +0300

    feat: -add view -add lib onChange

--- src/app.js
+++ src/app.js
@@ -1,2 +1,26 @@
-'use strict'
-import './app.css';
+import {MainView} from "./views/main/main.js";
+
+class App{
+    routes=[
+        {path:'',view:MainView}
+    ];
+    appState={
+        favorites:[]
+    }
+
+    constructor() {
+        //важно сбиндить его на текущий контекст, чтобы этот контекст не потерять
+        //конструктор который обрататывает смену хеша
+        window.addEventListener('hashchange',this.route.bind(this))
+        this.route()
+    }
+    route(){
+        if(this.currentView){
+            this.currentView.destroy();
+        }
+        const view=this.routes.find(r=>r.path==location.hash).view
+        this.currentView=new view(this.appState);
+        this.currentView.render();
+    }
+}
+new App();
